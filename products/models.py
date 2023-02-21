984c164ede6c6b25217dffa132bcd1d72978899e
t0by666 <70bytriplesix666@gmail.com>
2023-02-22 02:40:30 +0300

    added product db in products.views

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class ProductCategory(models.Model):
     def __str__(self):
         return self.name
     
-
+    
 class Product(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
@@ -16,3 +16,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Продукт - {self.name} | Категория - {self.category.name}"
