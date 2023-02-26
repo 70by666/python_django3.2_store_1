691edd5c6072a1a5d57399bd933c426c4d782c92
t0by666 <70bytriplesix666@gmail.com>
2023-02-26 10:28:43 +0300

    profile views and user profile form

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm, UserRegisterForm
+from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 
 
 def login(request):
@@ -15,7 +15,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserLoginForm()
         
@@ -36,3 +36,24 @@ def register(request):
     context = {'form': form}
     
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, 
+                               data=request.POST, 
+                               files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+        
+    context = {
+        'title': 'Store - Профиль', 
+        'form': form,
+    }
+    
+    return render(request, 'users/profile.html', context)
