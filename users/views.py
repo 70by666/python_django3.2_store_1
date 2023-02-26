e4f0b3455a960790796fd535258e4ea061db655e
t0by666 <70bytriplesix666@gmail.com>
2023-02-26 11:47:08 +0300

    logout and fixed nav bar dropdown

--- users/views.py
+++ users/views.py
@@ -58,3 +58,9 @@ def profile(request):
     }
     
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    
+    return HttpResponseRedirect(reverse('index'))
