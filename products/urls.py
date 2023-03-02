c139a5ec9a2a74857178d88d850d88f99c0f1990
t0by666 <70bytriplesix666@gmail.com>
2023-03-03 00:19:23 +0300

    filtering by category

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
+    path('category/<int:category_id>/', products, name='category'),
     path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
     path('basket/remove_one_item/<int:product_id>/', basket_remove_one_item, name='basket_remove_one_item'),
--- products/views.py
+++ products/views.py
@@ -1,36 +1,32 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
 
 from products.models import ProductCategory, Product, Basket
 
 
 def index(request):
-    context = {
-        'title': 'Store',
-    }    
+    context = {'title': 'Store'}    
     
     return render(request, 'products/index.html', context=context)
 
 
-def products(request):
+def products(request, category_id=None):   
     context = {
-        'title': 'Store - Каталог',
-        'products': Product.objects.all(),
-        'categories': ProductCategory.objects.all()
-    }   
+        'title': 'Store - Каталог', 
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.filter(category=category_id) if category_id else Product.objects.all(),
+    }
     
     return render(request, 'products/products.html', context=context)
 
 
 @login_required
 def basket_add(request, product_id):
-    product = Product.objects.get(id=product_id)
-    basket = Basket.objects.filter(user=request.user, product=product)
+    basket = Basket.objects.filter(user=request.user, product_id=product_id)
     if not basket.exists():
-        Basket.objects.create(user=request.user, product=product, quantity=1)
+        Basket.objects.create(user=request.user, product_id=product_id, quantity=1)
     else:
-        bsk = basket.first()
+        bsk = basket[0]
         bsk.quantity += 1
         bsk.save()
 
