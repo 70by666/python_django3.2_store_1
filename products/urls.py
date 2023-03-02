783dba807d4ff6269f88a723ccadeb31f2746912
t0by666 <70bytriplesix666@gmail.com>
2023-03-03 01:01:03 +0300

    Paginator

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,7 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='index'),
     path('category/<int:category_id>/', products, name='category'),
+    path('page/<int:page_num>', products, name='paginator'),
     path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
     path('basket/remove_one_item/<int:product_id>/', basket_remove_one_item, name='basket_remove_one_item'),
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from products.models import ProductCategory, Product, Basket
 
@@ -10,11 +11,14 @@ def index(request):
     return render(request, 'products/index.html', context=context)
 
 
-def products(request, category_id=None):   
+def products(request, category_id=None, page_num=1):
+    products = Product.objects.filter(category=category_id) if category_id else Product.objects.all()
+    paginator = Paginator(object_list=products, per_page=3)
+    products_paginator = paginator.page(page_num)
     context = {
         'title': 'Store - Каталог', 
         'categories': ProductCategory.objects.all(),
-        'products': Product.objects.filter(category=category_id) if category_id else Product.objects.all(),
+        'products': products_paginator,
     }
     
     return render(request, 'products/products.html', context=context)
