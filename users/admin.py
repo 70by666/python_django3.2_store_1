ac6b64377074afc84be2b62d951d8aebf5756e42
t0by666 <70bytriplesix666@gmail.com>
2023-03-03 03:47:12 +0300

    UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 from users.models import User
+from products.admin import BasketAdmin
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'id', 'first_name', 'last_name', 'email')
+    fields = ('username', ('first_name', 'last_name', 'email'), 
+              ('is_superuser', 'is_staff', 'is_active'), 
+              ('last_login', 'date_joined'), 'image')
+    search_fields = ('username', 'id', 'first_name', 'last_name', 'email')
+    readonly_fields = ('username', 'first_name', 'last_name', 'email', 
+                       'last_login', 'date_joined', 'image')
+    inlines = (BasketAdmin, )
