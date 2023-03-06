02c8f052ac428e6ef58e701feeb0299e371e19fe
t0by666 <70bytriplesix666@gmail.com>
2023-03-06 16:26:30 +0300

    EmailVerificationAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, EmailVerification
 from products.admin import BasketAdmin
 
 
@@ -14,3 +14,10 @@ class UserAdmin(admin.ModelAdmin):
     readonly_fields = ('username', 'first_name', 'last_name', 'email', 
                        'last_login', 'date_joined', 'image')
     inlines = (BasketAdmin, )
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'user', 'expiration')
+    fields = ('code', 'user', 'expiration', 'created')
+    readonly_fields = ('code', 'user', 'expiration', 'created')
