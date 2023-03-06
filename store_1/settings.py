89c2966ebf743a841327986c78ea132d35dd6409
t0by666 <70bytriplesix666@gmail.com>
2023-03-06 14:45:19 +0300

    EMAIL_BACKEND

--- store_1/settings.py
+++ store_1/settings.py
@@ -151,3 +151,7 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# send mail
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
