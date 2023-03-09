7c3eb604fd627ca302aca8ca8862679257206952
t0by666 <70bytriplesix666@gmail.com>
2023-03-09 22:03:17 +0300

    OrderForm

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+from orders.forms import OrderForm
+
+from common.views import CommonContextMixin
 
 
-class OrderCreateView(CreateView):
+class OrderCreateView(CommonContextMixin, CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('orders:create')
+    title = 'Store - Оформление заказа'
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super().form_valid(form)
